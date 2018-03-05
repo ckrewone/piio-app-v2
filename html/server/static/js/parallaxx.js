530284b535dc10d3e7fd220022e9600213153ed1
unknown <ckrewone@o2.pl>
2018-03-05 20:52:34 +0100

    Fix front end, add login and register forms

--- html/server/static/js/parallaxx.js
+++ html/server/static/js/parallaxx.js
@@ -1,4 +1,28 @@
 
+$("h2").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
+
+$("div").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
+
+$("header").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    },200);
+});
+
 $('.parallax-window').parallax();
 
 
@@ -62,15 +86,38 @@ $(document).ready(function(){
       $('#global-nv').addClass('scroll-nav');
 			$("#Logopng").addClass('scrolled-logo');
 			$('#Namepng').addClass('scrolled-name');
+			$(".nav-link").addClass('scrolled-link');
 
 
     } else if (scrollTop < 10) {
       $('#global-nv').removeClass('scroll-nav');
 			$('#Logopng').removeClass('scrolled-logo');
 			$('#Namepng').removeClass('scrolled-name');
-
+			$(".nav-link").removeClass('scrolled-link');
     }
 
   });
 
 });
+
+
+function loginView(){
+	$('#main').css({ display: 'none' });
+	$('#login').css({ display: 'block' });
+	$('#register').css({ display: 'none' });
+	$('.parallax-window').addClass('parallax-window-login');
+}
+
+function indexView(){
+	$('#main').css({ display: 'block' });
+	$('#login').css({ display: 'none' });
+	$('#register').css({ display: 'none' });
+	$('.parallax-window').removeClass('parallax-window-login');
+}
+
+function registerView(){
+	$('#main').css({ display: 'none' });
+	$('#register').css({ display: 'block' });
+	$('#login').css({ display: 'none' });
+	$('.parallax-window').addClass('parallax-window-login');
+}
